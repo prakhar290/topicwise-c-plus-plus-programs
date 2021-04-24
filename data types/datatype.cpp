4132501d5717b19b9fb71a2f17b4c03937802e7d
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-04-24 12:43:04 +0530

    Add files via upload

---
+++ data types/datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 = 5; //int is a integer datatype which we use to give input and take output as integer value
+    float num2 = 5.02;  //float datatype is a decimal/floating type, we use this for using decimal values as input/output
+    char alphabet = 'a'; //char datatype is used for using letters
+    bool num3=0; // this datatype can only take/give 0 and 1 as input/output 
+    cout<<"This number is of int datatype: "<<num1<<endl;
+    cout<<"This number is of float datatype: "<<num2<<endl;
+    cout<<"This is of char datatype: "<<alphabet<<endl;
+    cout<<"This is bool datatype: "<<num3<<endl;
+}
