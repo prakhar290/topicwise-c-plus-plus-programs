5a654321e6ac293bc7d4c2063f1b7c5fa01560a7
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-04-24 12:43:52 +0530

    Add files via upload

---
+++ data types/modifiers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //modifiers are used to increase the range of int/float datatypes
+    unsigned int ui;
+    signed int si;
+    long int li;
+    short int shi;
+    cout<<"the size of ui is: "<<sizeof(ui)<<endl;
+    cout<<"the size of si is: "<<sizeof(si)<<endl;
+    cout<<"the size of li is: "<<sizeof(li)<<endl;
+    cout<<"the size of shi is: "<<sizeof(shi)<<endl;
+    return 0;
+}
