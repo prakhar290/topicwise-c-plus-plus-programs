1de06225ac5316b11302c6cef7d00d6f14281fb9
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-04-25 14:19:33 +0530

    Add files via upload

---
+++ input output/input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // for taking input we use 'cin' followed by extraction operator '>>'.
+    int num1;
+    cout<<"enter number 1: ";
+    cin>>num1;
+    int num2;
+    cout<<"enter number 2: ";
+    cin>>num2;
+    int sum;
+    sum = num1 + num2;
+    cout<<"sum of two number is: ";
+    cout<<sum;
+    return 0;
+}
