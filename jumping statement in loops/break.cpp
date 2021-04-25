cdedb03cf000e7a69e9e6748977501127f946fab
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-04-25 20:31:33 +0530

    Add files via upload

---
+++ jumping statement in loops/break.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number to where you want to print the numbers: ";
+    cin>>num;
+    for(int i=0;i<num;i++)
+    {
+        if(i==6)
+        break;
+        else
+        cout<<i<<" ";
+    }
+    cout<<"\nthe number stops printing after 6 because we have used break statement in the loop.";
+    //break statement is used to take exit from the working loop and stop its furthur execution. 
+    return 0;
+}
