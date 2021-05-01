0fd05d6d0c14b48d6d22ee8feab9ed0e18cde614
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-01 21:14:03 +0530

    Add files via upload

---
+++ array programs/maxminofarray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter elements: ";
+    for(int i=0;i<n;i++)
+    { 
+        cin>>a[i];
+    }
+    int minno=INT_MAX;
+    int maxno=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        minno = min(minno,a[i]);
+        maxno = max(maxno,a[i]);
+    }
+    cout<<"minimum number of the array is: "<<minno;
+    cout<<"\nmaximum number of the array is: "<<maxno;
+    return 0;
+}
