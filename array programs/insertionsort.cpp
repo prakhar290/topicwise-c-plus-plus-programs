6a22e6f5a18c1a94467ed5fef180375f4d972a9f
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-02 21:53:48 +0530

    Add files via upload

---
+++ array programs/insertionsort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements of array: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=1;i<n;i++)
+    {
+        int current = a[i];
+        j = i-1;
+        while(a[j]>current && j>=0)
+        {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = current;
+    }
+    cout<<"array after sorting is: ";
+    for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    return 0;
+}
