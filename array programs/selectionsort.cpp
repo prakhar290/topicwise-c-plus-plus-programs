c00d00f1809be212b496593fc3de05a4dbf583a2
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-02 14:55:45 +0530

    Add files via upload

---
+++ array programs/selectionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            
+            if(a[j]<a[i])
+            {
+                int temp=a[j];
+                a[j] = a[i];
+                a[i]= temp;
+            }
+        }
+    }
+    cout<<"your array after sorting is: ";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
