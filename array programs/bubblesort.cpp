319747f16d1df7b7ea2f2c64b3336bdb58513cfe
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-02 21:05:22 +0530

    Add files via upload

---
+++ array programs/bubblesort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements of array: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    cout<<"array after sorting is: ";
+    for(i=0;i<n;i++)
+    cout<<a[i]<<" ";
+    return 0;
+}
