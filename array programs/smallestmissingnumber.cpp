08f480b5326e91b68a64745122c37ed9489da350
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-06 22:15:43 +0530

    Add files via upload

---
+++ array programs/smallestmissingnumber.cpp
@@ -0,0 +1,39 @@
+//you are given an array a[] of n integers including 0. the task is to find the smallest positive number missing from the array.
+//sample input: n =6 a[]={0,-9,1,3,-4,4}
+//sample output: 2
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,sm=0;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    cout<<"enter the elements: ";
+    int a[n];
+    
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        char c='f';
+        for(j=0;j<n;j++)
+        {
+            //cout<<j<<"  ";
+            if(a[j]==sm)
+            {
+                c = 'y';
+                break;
+            }
+            
+        }
+        if(c!='y')
+        {
+            cout<<sm;
+            return 0;
+        }
+        sm++;
+    }
+    return 0;
+}
