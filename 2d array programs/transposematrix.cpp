995235bd02ecb659a7385ad1c75ade31420e1535
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-09 13:45:38 +0530

    Add files via upload

---
+++ 2d array programs/transposematrix.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int rows,cols,i,j;
+    cout<<"enter the size of square matrix (row = columns): ";
+    cin>>rows>>cols;
+    int array[rows][cols];
+    cout<<"enter the elements of your 2d array: ";
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            cin>>array[i][j];
+        }
+    }
+    cout<<"printing your square matrix: \n";
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            cout<<array[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"the transpose of your matrix: \n";
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            cout<<array[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
