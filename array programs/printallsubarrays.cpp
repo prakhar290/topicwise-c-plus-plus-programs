49e38451f4b09fab6a680aed6bd07ff133eede8a
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-09 12:40:56 +0530

    Add files via upload

---
+++ array programs/printallsubarrays.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,j;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements of array: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"your all subarrays are:\n ";
+    for(i=0;i<n;i++)
+    {
+        for(int k=i;k<n;k++)
+        {
+            for(j=i;j<=k;j++)
+            {
+                cout<<a[j]<<" ";
+            }
+            cout<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
