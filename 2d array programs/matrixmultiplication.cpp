cd6c2eed55d644310805713ebbba499aec3e306a
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-09 14:00:09 +0530

    Add files via upload

---
+++ 2d array programs/matrixmultiplication.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r1,r2,c1,c2,i,j;
+    cout<<"enter the size of first matrix (row size first and then col size): ";
+    cin>>r1>>c1;
+    int array[r1][c1];
+    cout<<"enter the elements of your matrix: ";
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c1;j++)
+        {
+            cin>>array[i][j];
+        }
+    }
+    cout<<"printing your first matrix: \n";
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c1;j++)
+        {
+            cout<<array[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"enter the size of second matrix (row size first and then col size): ";
+    cin>>r2>>c2;
+    int b[r2][c2];
+    cout<<"enter the elements of your matrix: ";
+    for(i=0;i<r2;i++)
+    {
+        for(j=0;j<c2;j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+    cout<<"printing your second matrix: \n";
+    for(i=0;i<r2;i++)
+    {
+        for(j=0;j<c2;j++)
+        {
+            cout<<b[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int c[r1][c2];
+    if(c1==r2)
+    {
+        for(i=0;i<r1;i++)
+        {
+            for(j=0;j<c2;j++)
+            {
+                c[i][j]=0;
+                for(int k=0;k<c1;k++)
+                {
+                    c[i][j] = c[i][j] + array[i][k]*b[k][j]; 
+                }
+            }
+        }
+        cout<<"printing your matrix after multiplication: \n";
+        for(i=0;i<r1;i++)
+        {
+            for(j=0;j<c2;j++)
+            {
+                cout<<c[i][j]<<" ";
+            }
+            cout<<endl;
+        }  
+        return 0;      
+    }
+    else
+    cout<<"matrix multiplication is not possible.";
+    return 0;
+}
