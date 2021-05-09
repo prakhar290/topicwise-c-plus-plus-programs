940fd54f41375964420785a662ae5e8b82173bab
prakhar290 <68542024+prakhar290@users.noreply.github.com>
2021-05-09 12:59:30 +0530

    Add files via upload

---
+++ 2d array programs/elementsearching.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int rows,cols,i,j;
+    cout<<"enter the size of 2d array (row size first and then col size): ";
+    cin>>rows>>cols;
+    int array[rows][cols];
+    cout<<"enter the elements of your 2d array: ";
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            cin>>array[i][j];
+        }
+    }
+    int element;
+    cout<<"enetr the element to find its location: ";
+    cin>>element;
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            if(array[i][j]==element)
+            {
+                cout<<"element found at position "<<i+1<<" row and "<<j+1<<" column.";
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
