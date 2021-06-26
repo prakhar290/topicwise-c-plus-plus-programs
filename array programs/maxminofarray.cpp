58b9891a99736818241b05bd923ca89e690b6eba
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-06-26 23:13:44 +0530

    Update maxminofarray.cpp

--- array programs/maxminofarray.cpp
+++ array programs/maxminofarray.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 int main()
 {
+    
+    int a[100];
     int n;
     cout<<"enter the size: ";
     cin>>n;
-    int a[n];
     cout<<"enter elements: ";
     for(int i=0;i<n;i++)
     { 
