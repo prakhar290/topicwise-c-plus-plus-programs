4fbac45fdb52b1b9a47306ebabb40029bd1d5ac1
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-06-26 23:39:00 +0530

    Update subarrayques.cpp

--- array programs/subarrayques.cpp
+++ array programs/subarrayques.cpp
@@ -6,10 +6,11 @@
 using namespace std;
 int main()
 {
+    
+    int a[100],j;
     int n,s,i;
     cout<<"enter the size of array: ";
     cin>>n;
-    int a[n],j;
     cout<<"enter the elements: ";
     for(i=0;i<n;i++)
     {
