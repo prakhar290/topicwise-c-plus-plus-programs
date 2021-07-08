ae70744fb26c8d2d23c65beca46789acb2a833ea
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-08 23:56:51 +0530

    Add files via upload

---
+++ OOPs Concept/creating-destructor.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class student
+{
+    public:
+    string name;
+    int age;
+    char gender;
+
+    student(string s, int a, char g) //creating a constructor. It is called parameterized constructor
+    {
+        cout<<"parameteried constructor called.\n";
+        name = s;
+        age = a;
+        gender = g;
+    }
+
+    ~student() //destructor created. It will be called when our program ends. it destroy the object when it gets called
+    {
+        cout<<"Destructor called\n";
+        cout<<"object Destroyed.\n";
+    }
+    void print()
+    {
+        cout<<"name: "<<name<<endl;
+        cout<<"age: "<<age<<endl;
+        cout<<"gender: "<<gender<<endl;
+    }
+    
+};
+int main()
+{
+    student a("prakhar",21,'m'); //giving the values to the members of the class using constructor
+    //now to print the values we use the print function of the class
+    a.print(); 
+    return 0;
+}
