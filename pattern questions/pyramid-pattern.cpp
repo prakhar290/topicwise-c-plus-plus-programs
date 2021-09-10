a33052bd65965a9377ae0344da706cd5dcec9776
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-09-10 22:27:11 +0530

    Add files via upload

---
+++ pattern questions/pyramid-pattern.cpp
@@ -0,0 +1,36 @@
+/* printing pyramid pattern: 
+if n = 5, pattern will be like:
+               *
+              ***
+             *****
+            *******
+           *********
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter any number: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-1;j>=0;j--)
+        {
+            if(j<=i)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
